ab8ab09fa22e5b87e79e38384d74f943043bab05
Ali Elsokary <ali.elsokary8@gmail.com>
2021-12-25 17:03:25 +0200

    feat: Add UISegmentedControl Add AboutViewController with sample text

---
+++ FoursquarePlaces/Scenes/About/View Model/AboutViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  AboutViewModel.swift
+//  FoursquarePlaces
+//
+//  Created by Ali Elsokary on 25/12/2021.
+//  
+//
+
+import Foundation
+
+class AboutViewModel {
+
+	let aboutInfo = """
+					Space, the final frontier. These are the voyages of the Starship Enterprise. Its five-year mission:
+					to explore strange new worlds, to seek out new life and new civilizations,
+					to boldly go where no man has gone before. Many say exploration is part of our destiny,
+					but it’s actually our duty to future generations and their quest to ensure the survival of the human species.
+					"""
+}
---
+++ FoursquarePlaces/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,108 @@
+//
+//  HomeViewController.swift
+//  FoursquarePlaces
+//
+//  Created by Ali Elsokary on 25/12/2021.
+//  
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+	@IBOutlet private weak var segmentedControl: UISegmentedControl!
+	@IBOutlet private weak var viewContent: UIView!
+	
+	private enum ViewControllerIndex: Int {
+		case venuesViewController = 0
+		case aboutViewController = 1
+	}
+
+	private var currentViewController: UIViewController?
+
+	private lazy var venuesViewController: UIViewController? = {
+		let venuesViewController = self.storyboard?.instantiateViewController(withIdentifier: "VenuesViewController")
+		return venuesViewController
+	}()
+
+	private lazy var aboutViewController: UIViewController? = {
+		let aboutViewController = self.storyboard?.instantiateViewController(withIdentifier: "AboutViewController")
+		return aboutViewController
+	}()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		setupUI()
+	}
+
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		if let currentViewController = currentViewController {
+			currentViewController.viewWillDisappear(animated)
+		}
+	}
+
+}
+
+// MARK: - Actions
+
+private extension HomeViewController {
+	@IBAction func switchTabs(_ sender: UISegmentedControl) {
+		self.currentViewController!.view.removeFromSuperview()
+		self.currentViewController!.removeFromParent()
+
+		displayCurrentTab(sender.selectedSegmentIndex)
+	}
+}
+
+// MARK: - SetupUI
+
+private extension HomeViewController {
+	func setupUI() {
+		segmentedControl.setTitle("Venues", forSegmentAt: 0)
+		segmentedControl.setTitle("About", forSegmentAt: 1)
+		segmentedControl.selectedSegmentIndex = ViewControllerIndex.venuesViewController.rawValue
+		displayCurrentTab(ViewControllerIndex.venuesViewController.rawValue)
+		setNavigationTitle()
+	}
+
+	func setNavigationTitle(_ index: Int = 0) {
+		switch index {
+		case ViewControllerIndex.venuesViewController.rawValue:
+			self.title = "Venues"
+		case ViewControllerIndex.aboutViewController.rawValue:
+			self.title = "About"
+		default:
+			self.title = "Venues"
+		}
+	}
+}
+
+// MARK: - Configurations
+
+private extension HomeViewController {
+	func displayCurrentTab(_ index: Int) {
+		if let viewController = viewControllerForSelectedSegmentIndex(index) {
+			self.addChild(viewController)
+			viewController.didMove(toParent: self)
+
+			viewController.view.frame = self.viewContent.bounds
+			self.viewContent.addSubview(viewController.view)
+			self.currentViewController = viewController
+		}
+		setNavigationTitle(index)
+	}
+
+	func viewControllerForSelectedSegmentIndex(_ index: Int) -> UIViewController? {
+		var viewController: UIViewController?
+		switch index {
+		case ViewControllerIndex.venuesViewController.rawValue :
+			viewController = venuesViewController
+		case ViewControllerIndex.aboutViewController.rawValue :
+			viewController = aboutViewController
+		default:
+			return nil
+		}
+		return viewController
+	}
+}
